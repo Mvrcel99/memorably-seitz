c8380448fb778982666176460f28fdbe8804bfba
marcelbabel <babelmarcel5@gmail.com>
2026-03-12 22:08:35 +0100

    jonas du süßer habibi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+// Controller & Services
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Feature Modules
 import { HotelsModule } from './hotels/hotels.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { RoomsModule } from './rooms/rooms.module';
@@ -9,19 +15,35 @@ import { FeaturesModule } from './features/features.module';
 import { HotelImageModule } from './images/hotel-image/hotel-image.module';
 import { RoomImageModule } from './images/room-image/room-image.module';
 import { UsersModule } from './users/users.module';
+
 import { AuthModule } from './_common/auth/auth.module';
 import { CaslModule } from './_common/casl/casl.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
+    
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      
+      synchronize: process.env.NODE_ENV !== 'production',
+      entities: [join(__dirname, '**', '*.entity{.ts,.js}')],
     }),
+
+    
+    ServeStaticModule.forRoot({
+      
+      rootPath: join(process.cwd(), 'uploads'),
+      
+      serveRoot: '/api/v1/images',
+      serveStaticOptions: {
+        index: false, 
+        cacheControl: true, 
+        maxAge: 31536000, 
+      },
+    }),
+
+
     HotelsModule,
     BookingsModule,
     RoomsModule,
@@ -31,16 +53,6 @@ import { join } from 'path';
     UsersModule,
     AuthModule,
     CaslModule,
-    ServeStaticModule.forRoot({
-      
-      rootPath: join(process.cwd(), 'uploads'),
-
-      serveRoot: '/images',
-     
-      serveStaticOptions: {
-        index: false,
-      },
-    }),
   ],
   controllers: [AppController],
   providers: [AppService],
